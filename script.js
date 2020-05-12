12b1e4a4086db39fd601c22fa4237ec2b4c82938
Carine Becker <caarine.m@gmail.com>
2020-05-12 14:59:39 -0300

    first
    Signed-off-by: Carine Becker <caarine.m@gmail.com>

---
+++ script.js
@@ -0,0 +1,15 @@
+const buttonElement = document.querySelector('button')
+
+buttonElement.onclick = function() {
+
+    document.documentElement.style.setProperty('--primary-color', getRandomColor())
+}
+
+function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+ };
